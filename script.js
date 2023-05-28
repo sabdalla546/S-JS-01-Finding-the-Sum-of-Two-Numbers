a2c7ed268b358c673307c96d3831009ea4163590
sabdalla546 <64744897+sabdalla546@users.noreply.github.com>
2023-05-28 12:05:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function add() {
+    const num1 = document.getElementById('first-number').value;
+    const num2 = document.getElementById('sacond-number').value;
+    const result = Number(num1) + Number(num2);
+    alert(`Result = ${result}`);
+}
